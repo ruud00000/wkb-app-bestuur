6a1e12e7084be9e1c9c28b6cddf5b1762afaacd7
ruud00000 <ruudnaastepad@hotmail.com>
2024-03-15 14:46:14 +0100

    uploadfile current year

--- validator.js
+++ validator.js
@@ -1,5 +1,7 @@
 export const isValidFile = (uploadFile) => {
+  const d = new Date();
+  let year = d.getFullYear();
   console.log('uploadFile.name: ', uploadFile.name);
-  return uploadFile.name.includes('Competitie 2023') && uploadFile.name.includes('ronde') && uploadFile.name.includes('.xlsx');
+  return uploadFile.name.includes('Competitie') && uploadFile.name.includes(year) && uploadFile.name.includes('ronde') && uploadFile.name.includes('.xlsx');
 }
 
